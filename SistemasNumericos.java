759d356cdc332cedb637b385c951559e85953e65
DevEd <deved@MacBook-Pro-de-DevEd.local>
2024-06-03 16:18:50 -0600

    Sistemas Numericos en Java

---
+++ SistemasNumericos.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+
+public class SistemasNumericos {
+	public static void main(String[] args) {
+		
+		String numeroStr = JOptionPane.showInputDialog(null, "Ingrese un número decimal:");
+		int numeroDecimal = Integer.parseInt(numeroStr); 
+		System.out.println("numeroDecimal = "+ numeroDecimal);
+		
+		System.out.println("numero binario de "+ numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal));
+		
+		int numeroBinario = 0b111110100;
+		System.out.println("numeroBinario = "+ numeroBinario);
+		
+		System.out.println("numero octal de "+ numeroBinario + " = " + Integer.toOctalString(numeroDecimal));
+		int numeroOctal = 0764;
+		System.out.println("numero octal =  "+ numeroOctal );
+		System.out.println("numero hexadecimal de "+ numeroDecimal + " = " + Integer.toHexString(numeroDecimal) );
+		
+		int numeroHex = 0x11f4;
+		System.out.println("numeroHex =  "+ numeroHex );
+		System.out.println("\n\n");
+		
+		String mensaje = "numero binario de "+ numeroDecimal + " = " + Integer.toBinaryString(numeroDecimal);
+		mensaje += "\nnumero octal de "+ numeroBinario + " = " + Integer.toOctalString(numeroDecimal);
+		mensaje += "\nnumero hexadecimal de "+ numeroDecimal + " = " + Integer.toHexString(numeroDecimal);
+		JOptionPane.showMessageDialog(null,mensaje);
+		
+	}
+}
